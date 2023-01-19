4f838ea5a0736eec8b798ac7c1d7c97a14054953
OsasereImade <osasereimade@gmail.com>
2023-01-19 18:15:12 -0500

    First Commit

--- Project1/src/project1/Project1.java
+++ Project1/src/project1/Project1.java
@@ -1,69 +1,51 @@
 package project1;
 
-import java.util.StringTokenizer;
-
 public class Project1 {
-   public static TextFileInput myFile;
-   public static StringTokenizer myTokens;
-   public static String line;
-   public static String sentence;
-   public static String [] input;
-   public static char upperCaseVowels;
-   public static char lowerCaseVowels;
-   public static int length;
-   public static int distance;
-   public static int meter;
-   public static int kilometer;
-   public static int howLong;
-   public static int total;
-   
 
-    public static void main(String[] args) {
-       // input = new String [];
-        length = 0;
-        distance = 0;
-        meter = 0;
-        kilometer = 0;
-        howLong = 0;
-        total = 0;
-        myFile = new TextFileInput("input.txt");
-        line = myFile.readLine();
-        while(line != null) {
-        //System.out.println("The first line is " + line);
-        for(int i = 0; i <line.length(); i++) {
-           if(line.charAt(i) == 'A' || line.charAt(i) == 'a') length++;
-           else if (line.charAt(i) == 'E' || line.charAt(i) == 'e') kilometer++;
-           else if (line.charAt(i) == 'i' || line.charAt(i) == 'I') distance++;
-           else if (line.charAt(i) == 'o' || line.charAt(i) == '0') meter++;
-           else if (line.charAt(i) == 'u' || line.charAt(i) == 'U') howLong++;
-
-        total = length + kilometer + distance + meter + howLong;
+   public static void main(String[] args) {
+      String line;
+      TextFileInput in = new TextFileInput(args[0]);
       
-        } //for loop
-        System.out.println("There are: " + total  +" letter a vowels in  line 1");
-        System.out.println("There are: " + total + " letter a vowels in the line 2");
-        System.out.println("There are: " + total + " letter a vowels in the line 3");
-        System.out.println("There are: " + total + " letter a vowels in the line 4");
-        System.out.println("There are: " + total + " letter a vowels in the line 5");
-        
-        myFile.close();
-        }
-//        System.out.println("There are: " + length + " letter a vowels in the line");
-//            distance = 0;
-//            line = myFile.readLine();
-//            System.out.println("The second line is " + line);
-//            for(int i = 0; i <line.length(); i++) {
-//                if(line.charAt(i) == 'e'|| line.charAt(i) == 'E') {
-//                    distance++;
-//                }
-//            } 
-//            System.out.println("There are: " + distance + " letter a vowels in the line");
-//                else if (line.charAt(i) == 'i' || line.charAt(i) == 'I')
-//                else if (line.charAt(i) == 'o' || line.charAt(i) == '0')
-//                else if (line.charAt(i)== 'u' || line.charAt(i) == 'U');
-//                length++;
-             
-        //System.out.println("There are " + length + " letter a vowels in the line");
-    }// main
-}//class
+      // read the file to know how many line are in the file
+      int size = 0;
+      line = in.readLine();
+      while (line != null) {
+         size++;
+         line = in.readLine();
+      }// end of while loop
+      
+      // puts the originalText and count 
+      // in a string size array
+      String[] originalText = new String[size];
+      String[] count = new String[size];
 
+      // reads each line in the file
+      in = new TextFileInput(args[0]);
+      line = in.readLine();
+      int currentLine = 0;
+      while(line != null) {
+         // initializes counters to increment any time a
+         // vowel is encountered
+         int countA = 0, countE = 0, countI = 0, countO = 0, countU = 0;
+         for(int i = 0; i < line.length(); i++) {
+            if (line.charAt(i) == 'A' || line.charAt(i) == 'a') countA++;
+            else if(line.charAt(i) == 'E' || line.charAt(i) == 'e') countE++;
+            else if(line.charAt(i) == 'I' || line.charAt(i) == 'i') countI++;
+            else if (line.charAt(i) == 'O' || line.charAt(i) == 'o') countO++;
+            else if (line.charAt(i) == 'U' || line.charAt(i) == 'u') countU++;
+            originalText[currentLine] = line;
+         }//end of for loop
+         
+         line = in.readLine();// reads the file
+         count[currentLine] = countA + " " + countE + " " + countI + " " + countO + " " + countU;
+         currentLine ++;
+      }//end of while loop
+      
+      
+      VowelGUI.printVOWELtoJFrame(new VowelGUI(), originalText, count);
+
+      in.close();
+
+   }//close main
+
+}//close class
